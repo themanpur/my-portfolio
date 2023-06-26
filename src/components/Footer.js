e17e684e1780d1171f3fefdd15e4562da95663e3
themanpur <jeudong_gervais@yahoo.com>
2023-06-26 08:40:00 +0100

    update for mobile 1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,29 @@
-import {Container, Row, Col, Nav, Navbar, Badge} from 'react-bootstrap';
+import {Container, Row, Col, Navbar, Badge} from 'react-bootstrap';
 
 function Competences () {
     return (
         <Container id='competence' className='bg-light border-top border-dark' fluid>
-            <Row className='md-p-3 align-items-center'>
-                <Col md={2}>
+            <Row className='p-5 align-items-center'>
+                <Col xs={12} md={4}>
                     <Navbar.Brand href="#">
                         <Badge className='bg-dark' >THEMAN</Badge> MOBALI
                     </Navbar.Brand>
                 </Col>
-                <Col md={8}>
+                <Col xs={12} md={4}>
                     Tous droits reserves
                 </Col>
-                <Col md={2}>
-                    <Nav>
-                        <Nav.Link href="https://www.linkedin.com/in/gervais-jeudong">
-                        <i className='bx bxl-linkedin bx-sm text-primary'></i>
-                        </Nav.Link>
-                        <Nav.Link href="https://twitter.com/themobali?t=2joQH06Bsv9gGaxtlUntAQ&s=09">
-                        <i className='bx bxl-twitter bx-sm text-primary'></i>
-                        </Nav.Link>
-                        <Nav.Link href="http://wa.me/655734950">
-                        <i className='bx bxl-whatsapp bx-sm text-success'></i>
-                        </Nav.Link>
-                    </Nav>
+                <Col xs={12} md={4}>
+                    <Row className='p-2 align-items-center d-inline'>
+                            <a href="https://www.linkedin.com/in/gervais-jeudong">
+                            <i className='bx bxl-linkedin bx-sm text-primary'></i>
+                            </a>
+                            <a href="https://twitter.com/themobali?t=2joQH06Bsv9gGaxtlUntAQ&s=09">
+                            <i className='bx bxl-twitter bx-sm text-primary'></i>
+                            </a>
+                            <a href="http://wa.me/655734950">
+                            <i className='bx bxl-whatsapp bx-sm text-success'></i>
+                            </a>
+                    </Row>
                 </Col>
             </Row>
         </Container>
